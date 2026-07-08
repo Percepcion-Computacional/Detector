55ca0204b62edd7fd62f8e5f09d5b814dfa00252
Emerson-Lezama-Vera <emerson_00_35@hotmail.com>
2026-07-07 23:00:43 -0500

    Update CORS configuration to allow Vercel frontend

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ajustar en producción
+    allow_origins=[
+        "http://localhost:3000",
+        "https://detector-frontend-ten.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
